6cd8748000bbafe5dee3a2b17b991b9dbbc3c1da
Kaung Myat Hun <kaungmyathun7@gmail.com>
2022-12-05 03:01:25 +0630

    Rout to all Path

--- src/components/routes/Router.jsx
+++ src/components/routes/Router.jsx
@@ -6,14 +6,22 @@ import Error from '../Error/Error'
 import Profile from '../Setting/Profile/Profile'
 import Myanmar3d from '../Myanmar3d/Myanmar3d'
 import About from '../AboutUs/About'
+import LiveFootball from '../LiveFootball/LiveFootball'
+import WinnerList from '../WinnerList/WinnerList'
+import Myanmar2d from '../2D/TwoD'
+import FootballBetting from '../FootballBetting/FootballBetting'
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={ <Home/> }></Route>
+      <Route path="/footballbetting" element={<FootballBetting/>}></Route>
+      <Route path="/livefootball" element={<LiveFootball/>}></Route>
       <Route path="/setting" element={ <Setting/> }></Route>
       <Route path="setting/profile" element={<Profile/>}></Route>
+      <Route path="/myanmar2d" element={<Myanmar2d/>}></Route>
       <Route path="/myanmar3d" element={<Myanmar3d/> }></Route>
+      <Route path="/winnerlist" element={<WinnerList/>}></Route>
       <Route path="/shwelarb" element={<About/>}></Route>
 
       {/* Error */}
