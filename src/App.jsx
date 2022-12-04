e6409d95e7b99039f5d4005953f1974f3fefcd8a
thazinphyoe <thazinphyoe455@gmail.com>
2022-12-04 15:12:35 +0630

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
+
 import './App.css'
-
+import ColorDescription from './components/2D/ColorDescription/ColorDescription'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <h2 className='text-teal-500'>HIHfiag</h2>
-      <button className="btn btn-secondary">Button</button>
-      <button className="btn btn-accent">Button</button>
+    <div className='app'>
+<ColorDescription></ColorDescription>
     </div>
   )
 }
---
+++ src/components/2D/ColorDescription/ColorDescription.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const ColorDescription = () => {
+  return (
+    <div className='h-screen container bg-white text-black'>
+        <h1 className='text-center font-bold text-2xl'>ColorDescription</h1>
+        <div className='flex items-center px-5'>
+            <div className='w-20 h-20 bg-slate-500 rounded-full'></div>
+            <p className='font-bold ml-7 text-xl'>0%-25%</p>
+        </div>
+        <div className='flex items-center px-5 mt-3'>
+            <div className='w-20 h-20 bg-yellow-300 rounded-full'></div>
+            <p className='font-bold ml-7 text-xl'>25%-50%</p>
+        </div>
+        <div className='flex items-center px-5 mt-3'>
+            <div className='w-20 h-20 bg-lime-700 rounded-full'></div>
+            <p className='font-bold ml-7 text-xl'>50%-75%</p>
+        </div>
+        <div className='flex items-center px-5 mt-3'>
+            <div className='w-20 h-20 bg-blue-500 rounded-full'></div>
+            <p className='font-bold ml-7 text-xl'>75%-100%</p>
+        </div>
+   
+        <div className='flex items-center px-5 mt-3'>
+            <div className='w-20 h-20 bg-red-600 rounded-full'></div>
+            <p className='font-bold ml-7 text-xl'>100%</p>
+        </div>
+        <div className='flex items-center px-5 mt-3'>
+            <div className='w-20 h-20 bg-cyan-300 rounded-full'></div>
+            <p className='font-bold ml-7 text-xl'>ဆတူ R</p>
+        </div>
+        </div>
+  )
+}
+
+export default ColorDescription
