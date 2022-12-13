1ee071f4572d043a1bf0812764cb8a385ce9c069
thazinphyoe <thazinphyoe455@gmail.com>
2022-12-13 12:30:30 +0630

    eye

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,23 +1,30 @@
-import React,{useState,useRef} from 'react'
+import React,{useState,useRef,useEffect} from 'react'
 import classes from './Login.module.css'
 import logo from '../../assets/shwelarbh.png'
 import {useNavigate} from 'react-router-dom'
+import { AiFillEye ,AiFillEyeInvisible} from 'react-icons/ai';
+
 import axios from 'axios'
 
 const Login = () => {
+  const [open,setOpen] = useState(false);
   let navigate = useNavigate();
   const [email,setEmail] = useState('')
   const [password,setPassword] = useState('')
   const emailRef = useRef()
-  const passwordRef = useRef()
+useEffect(()=>{
+  emailRef.current.focus()
+
+},[])
+  const emailChangeHandler = (e)=>{
+   setEmail(e.target.value)
+  }
+  const passwordChangeHandler = (e)=>{
+  setPassword(e.target.value)
+ }
   const {VITE_APP_DOMAIN} = import.meta.env;
- 
-
-
-  const loginHandler = (e)=>{
+ const loginHandler = (e)=>{
     e.preventDefault()
-    setEmail(emailRef.current.value);
-    setPassword(passwordRef.current.value)
  axios.post(`${VITE_APP_DOMAIN}/api/login`,{
   email,
   password
@@ -28,7 +35,8 @@ const Login = () => {
     localStorage.setItem('lToken', res.data.data['l_token']);
     navigate('/home')
 }).catch(err=>{
-  console.log(err);
+  console.log(err
+    );
 })
   }
 
@@ -39,11 +47,21 @@ const Login = () => {
     <img src={logo} className='w-40 flex justify-center items-center'></img>
   </div>
   <form onSubmit={loginHandler}>
-  <input type='text' placeholder='Enter Email or Phone' className='input mb-5 w-full focus:outline-none' ref={emailRef}></input>
-  <div>
-   <input type='password' placeholder='Password' className='input mb-5 w-full' ref={passwordRef}></input>
-   <div></div>
+  <input type='text' placeholder='Email or Phone Number' className='input mb-5 w-full focus:outline-none' ref={emailRef} onChange={emailChangeHandler}></input>
+  
+  <div className='relative'>
+    <div>
+    <input type={open?'text':'password'} placeholder='Password' onChange={passwordChangeHandler} className='input mb-5 w-full'></input>
     </div>
+   <div className='absolute top-3 right-4 text-xl'>
+    {
+      open?<AiFillEye onClick={()=>setOpen(!open)}></AiFillEye>:<AiFillEyeInvisible onClick={()=>setOpen(!open)}></AiFillEyeInvisible>
+    }
+
+    
+
+   </div>
+  </div>
       <button className={`${classes.btn} btn btn-md `}>Log in</button>
   </form>
   <a className={classes.pass}>Forgot Password?</a>
