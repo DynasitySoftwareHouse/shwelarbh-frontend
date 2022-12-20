ceb10fc00a9db85e332b48085aa387fdaa703d6f
Kaung Myat Hun <kaungmyathun7@gmail.com>
2022-12-21 02:17:03 +0630

    change: ui

--- src/components/Setting/TransitionHistory/TransitionHistory.jsx
+++ src/components/Setting/TransitionHistory/TransitionHistory.jsx
@@ -27,44 +27,46 @@ function TransitionHistory() {
         console.log(error);
       });
   }, [`${VITE_APP_DOMAIN}/api/wallet/transaction-histories?user_id=${userData?.user_id}`]);
-  console.log(transactionHistories);
+
   return (
     <div className={style.mainContainer}>
-      <table className=" text-center table sm:w-96 md:w-11/12 m-auto">
-        <thead className=" text-white">
-          <tr className="bg-red-700">
-            <td className="bg-red-700">Date</td>
-            <td className="bg-red-700">Amount</td>
-            <td className="bg-red-700">Type</td>
-            <td className="bg-red-700">Description</td>
-          </tr>
-        </thead>
-        <tbody>
-          {transactionHistories ? (
-            transactionHistories.map((item) => (
-              <tr
-                key={item.id}
-                className={`${item.type === "increase" ? "text-lime-600" : "text-red-600"}`}>
-                <td>{new Date(item.created_at).toLocaleDateString()}</td>
-                <td>{item.amount}</td>
-                <td>{item.type}</td>
-                <td>{item.note}</td>
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan="4" className="bg-white h-96">
-                <p className="text-center text-black">There is no data</p>
-              </td>
+      <div className={style.tableContainer}>
+        <table className={`${style.table} text-center table sm:w-96 md:w-11/12 m-auto`}>
+          <thead className=" text-white">
+            <tr className="bg-red-700">
+              <td className="bg-red-700">Date</td>
+              <td className="bg-red-700">Amount</td>
+              <td className="bg-red-700">Type</td>
+              <td className="bg-red-700">Description</td>
             </tr>
-          )}
-          {/* <tr>
+          </thead>
+          <tbody>
+            {transactionHistories ? (
+              transactionHistories.map((item) => (
+                <tr
+                  key={item.id}
+                  className={`${item.type === "increase" ? "text-lime-600" : "text-red-600"}`}>
+                  <td>{new Date(item.created_at).toLocaleDateString()}</td>
+                  <td>{item.amount}</td>
+                  <td>{item.type}</td>
+                  <td>{item.note}</td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="4" className="bg-white h-96">
+                  <p className="text-center text-black">There is no data</p>
+                </td>
+              </tr>
+            )}
+            {/* <tr>
             <td colSpan="4" className="bg-white h-96">
               <p className="text-center text-black">There is no data</p>
             </td>
           </tr> */}
-        </tbody>
-      </table>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
