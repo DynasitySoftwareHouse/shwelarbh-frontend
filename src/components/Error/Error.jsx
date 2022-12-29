4d4106468d0086f0450fcd0a6dbd90e6aeb93edf
Kaung Myat Hun <kaungmyathun7@gmail.com>
2022-12-29 15:04:17 +0630

    env: dynamic project

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import error from "../../assets/404error.jpg";
 
 function Error() {
   return (
-    <div>
-      Error 404! <br /> Not Found
+    <div
+      style={{
+        height: "100vh",
+
+        display: "flex",
+        justifyContent: "center",
+        alignItem: "center",
+      }}>
+      <img src={error} alt="" style={{ width: "100vw", height: "100%" }} />
     </div>
-  )
+  );
 }
 
-export default Error
+export default Error;
